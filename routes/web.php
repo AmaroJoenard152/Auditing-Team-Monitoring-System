0b9edae282467359b952c09e732b1df963270f71
joenardamaro <joenardamaro152@gmail.com>
2025-01-25 18:54:46 +0800

    INT-16: Created a Search Function in One Time Cleansing (PPE)

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,4 @@ Route::get('/ppe', [PpeController::class, 'showPpe'])->name('inventory.ppe');
 Route::get('/deletePpe/{id}', [PpeController::class, 'deletePpe'])->name('inventory.ppe');
 Route::get('/updatePpe/{id}', [PpeController::class, 'updatePpe'])->name('inventory.ppe');
 Route::post('/savePpe/{id}', [PpeController::class, 'savePpe'])->name('inventory.ppe');
+Route::get('/searchPpe', [PpeController::class, 'searchPpe'])->name('inventory.ppe');

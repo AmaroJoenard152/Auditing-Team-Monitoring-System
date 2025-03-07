0532cdf9959332b68ae3e7cb1eaa47087affdd47
joenardamaro <joenardamaro152@gmail.com>
2025-03-07 15:36:50 +0800

    Complete the develop branch

--- routes/web.php
+++ routes/web.php
@@ -41,4 +41,10 @@ Route::get('/deletePpe/{id}', [PpeController::class, 'deletePpe'])->name('invent
 Route::get('/updatePpe/{id}', [PpeController::class, 'updatePpe'])->name('inventory.ppe');
 Route::post('/savePpe/{id}', [PpeController::class, 'savePpe'])->name('inventory.ppe');
 Route::get('/searchPpe', [PpeController::class, 'searchPpe'])->name('inventory.ppe');
+Route::get('/export-csv', [PpeController::class, 'dvExportCSV'])->name('export.csv');
+
+Route::get('/ppe', [PpeController::class, 'showPpe'])->name('inventory.ppe');
 Route::get('/ppe/history', [PpeController::class, 'showPpe']); // Use the same function for showPpe and showPpeHistory
+
+
+
